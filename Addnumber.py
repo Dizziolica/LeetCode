cc58718b9aff0002fb4599e489c8d143b9c1680d
Caroline Oliveira <104214541+Dizziolica@users.noreply.github.com>
2022-11-16 20:17:49 +0000

    First Exercises

---
+++ Addnumber.py
@@ -0,0 +1,34 @@
+def addnumber(list1, list2):
+    soma = 0
+    newlist = []
+  
+    for i in list1:
+      
+           
+
+      if list1 is not None:
+                 
+        soma +=i
+        newlist1 = newlist.append(i)
+       
+        
+    for j in list2:
+      
+      if list2 is not None:
+
+        soma += j 
+     
+        newlist1 = newlist.append(j)
+            
+            
+                 
+                 
+                 
+           
+               
+
+    print(soma)
+   
+    print(newlist)
+
+
