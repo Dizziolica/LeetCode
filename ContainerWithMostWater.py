a30ebcd80c88f58a2646889edaa8c69b511de862
Caroline Oliveira <104214541+Dizziolica@users.noreply.github.com>
2022-12-14 16:34:13 +0000

    Update ContainerWithMostWater.py

--- ContainerWithMostWater.py
+++ ContainerWithMostWater.py
@@ -1,25 +1,28 @@
-list, maxnum1, maxnum2 = [1, 8, 3, 4, 5, 7], 0, 0
-newlist2 = []
+def Container(list):
+    maxnum1, maxnum2 =  0, 0
+    newlist2 = []
 
-for i in list:
+    for i in list:
     
-    if i > maxnum1:
-        maxnum1 = i
+        if i > maxnum1:
+            maxnum1 = i
       
-    else:
-        continue
+        else:
+            continue
 
 
 
-newlist2.append(maxnum1)
-newlist = list.copy()
-newlist.remove(maxnum1)
-maxnum2 = max(newlist)
-newlist2.append(maxnum2)
+    newlist2.append(maxnum1)
+    newlist = list.copy()
+    newlist.remove(maxnum1)
+    maxnum2 = max(newlist)
+    newlist2.append(maxnum2)
 
-result = min(newlist2)  *  (list.index(maxnum2)  - list.index(maxnum1))
-
-
+    result = min(newlist2)  *  (list.index(maxnum2)  - list.index(maxnum1))
 
 
-print(result)
+
+
+    print(result)
+    
+Cointainer()
